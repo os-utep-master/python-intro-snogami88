ba57979ba84a589f0cb2ffd8240841dea2fe77d1
Sergio-pc <Sergio-pc@DESKTOP-FHON1S2>
2019-08-29 21:05:14 -0600

    Testing and deugging

--- wordCount.py
+++ wordCount.py
@@ -24,31 +24,33 @@ if not os.path.exists(output_file_name):
 
 #Read the file
 def read_file():    
-    with open (input_file_name, 'r') as inputF           #open and read the file then close
+    with open (input_file_name, 'r') as inputF      #open and read the file then close
         word_list = inputF.read()                   #Read the complete file
         word_list = word_list.casefold()            #Do not compare the case tense
         word_list = word_list.split()               #Remove any next line spaces
-        word_list = re.split('[\W+]', word_list)    #split into array of anything that is 
+        word_list = re.split('[\W+]', word_list)    #split into array of anything that is not a word
         word_list = sorted(set(word_list))          #Put list in alphabetical order
         print(word_list)
     return word_list
 
 #Print output file, or overide existing file
 def create_file(final_list, num_list):
-    with open (output_file_name, 'w') as inputOut
+    with open (output_file_name, 'w') as inputOut   #open file write and close file
+        i = 0
         for e in final_list
-            print("%s %d\n", (final_list, num_list))  
+            inputOut.write("%s %d\n", (e, num_list[i])
+            i = i + 1
 
-#Create array with count of each word
+#Create array with count of each words
 def get_count(words):
     i = 0
     prev = ""
     for index in words:
         if prev != index:
-            i++
+            i = i + 1
         count[i] = count + 1
         prev = index
-        
+
 
 word_list = read_file()
 count = organize(word_list)
