a21f04c5dc60d4916e712e59a8ffc9d7da2300c8
Sergio-pc <Sergio-pc@DESKTOP-FHON1S2>
2019-08-29 19:03:13 -0600

    "New Editor"

--- wordCount.py
+++ wordCount.py
@@ -10,17 +10,18 @@ if(len(sys.argv) is not 3):
 input_file_name = sys.argv[1]
 output_file_name = sys.argv[2]
 
-#
+#Check if input file exists
 if not os.path.exists(input_file_name):
     print("input text file %s does not exists!" % input_file_name)
     exit()
-#
+
+#Check if output file exists
 if not os.path.exists(output_file_name):
     print("output text file %s does not exists!" % output_file_name)
     exit
 
 #Read the file
-def read_file(input_file):
+def read_file(input_file):    
 
 #Print output file, or overide existing file
 def create_file(final_list):
@@ -39,5 +40,5 @@ file_info = read_file(input_file_name)
 word_list = create_list(file_info)
 word_list = organize(word_list)
 create_file(word_list)
-print("Information from %s has been seperated into a list posted in %s" input_file_name, output_file_name)
+print("Information from %s has been seperated into a list posted in %s" (input_file_name, output_file_name)
 
